16f602b26db1ee74fdb108ba517e6bb06903b667
Erik Anderson <108945295+ErikAndersonWebDev@users.noreply.github.com>
2022-11-26 22:00:57 -0800

    All JS practice so far

---
+++ ice-cream.js
@@ -0,0 +1,10 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push("Root Beer");
+
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);
+
